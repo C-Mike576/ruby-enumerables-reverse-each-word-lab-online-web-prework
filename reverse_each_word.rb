d2ccc64cf2c6b385ea1566ef11927ff4393fe32b
Michael Calkins <m.calkins576@gmail.com>
2019-10-23 16:18:55 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,6 +2,6 @@ def reverse_each_word(input)
   output = []
   output << input.split(/ /)
   output[0].reverse!
-  output[0].reverse_each {|x| print x, " " }
+  output[0].reverse_each {|x| p x, " " }
   
 end
